a110be2bab40bdee628c52dbca91dd768034b236
Nathan Holmes <nholmes@email.unc.edu>
2022-04-25 11:49:58 -0400

    Working, but not buttons

--- public/main.js
+++ public/main.js
@@ -1,8 +1,107 @@
 // Focus div based on nav button click
+document.getElementById('homenav').onclick = function() {
+    document.getElementById('homenav').className = 'active'
+    document.getElementById('singlenav').className = 'hidden'
+    document.getElementById('multinav').className = 'hidden'
+    document.getElementById('guessnav').className = 'hidden'
+}
+document.getElementById('homenav').onclick = function() {
+    document.getElementById('home').className = 'hidden'
+    document.getElementById('one').className = 'active'
+    document.getElementById('many').className = 'hidden'
+    document.getElementById('guess').className = 'hidden'
+}
+document.getElementById('homenav').onclick = function() {
+    document.getElementById('home').className = 'hidden'
+    document.getElementById('one').className = 'hidden'
+    document.getElementById('many').className = 'active'
+    document.getElementById('guess').className = 'hidden'
+}
+document.getElementById('homenav').onclick = function() {
+    document.getElementById('home').className = 'hidden'
+    document.getElementById('one').className = 'hidden'
+    document.getElementById('many').className = 'hidden'
+    document.getElementById('guess').className = 'active'
+}
 
 // Flip one coin and show coin image to match result when button clicked
+function flipCoin() {
+    fetch('http://localhost:5000/app/flip/')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            document.getElementById("outcome").innerHTML = result.flip;
+            document.getElementById("image").setAttribute("src", "assets/img/" + result.flip + ".png");
+        })
+}
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+function flipCoins() {
+    numberCoins = document.getElementById("number").value;
+    fetch('http://localhost:5000/app/flips/coins', {
+        body: JSON.stringify({
+            "number": number
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "post"
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            document.getElementById("totalHeads").innerHTML = result.summary.heads;
+            document.getElementById("totalTails").innerHTML = result.summary.tails;
 
+            var tableBody = document.getElementById("data");
+            for (var i = 0; i < result.raw.length; i++) {
+                var row = document.createElement("tr");
+                var counter = document.createElement("td");
+                var result = document.createElement("td");
+                var cell = document.createElement("td");
+                var image = document.createElement("img");
+
+                counter.innerHTML = i + 1;
+                result.innerHTML = result.raw[i];
+
+                image.setAttribute("src", "assets/img/" + result.raw[i] + ".png");
+                image.setAttribute("class", "smallcoin");
+
+                row.appendChild(counter);
+                row.appendChild(result);
+                cell.appendChild(image);
+                row.appendChild(cell);
+                tableBody.appendChild(row);
+            }
+            document.getElementById("resultTable").setAttribute("class", "active");
+        })
+}
 // Guess a flip by clicking either heads or tails button
+function guessTails() {
+    fetch('http://localhost:5000/app/flip/call/tails')
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+            document.getElementById("outcome").setAttribute("src", "/assets/img/" + result.flip + ".png");
+            document.getElementById("result").innerHTML = "You " + result.result + ".";
+        })
+}
+
+function guessHeads() {
+    fetch('http://localhost:5000/app/flip/call/heads')
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+            document.getElementById("outcome").setAttribute("src", "/assets/img/" + result.flip + ".png");
+            document.getElementById("result").innerHTML = "You " + result.result + ".";
+        })
+}
--- src/routes/someroutes.js
+++ src/routes/someroutes.js
@@ -13,12 +13,7 @@ someroutes.route('/app/flip/').get(function (req, res, next) { // Flip a coin an
     // Flip a coin using the coinFlip() function
     const result = coins.coinFlip();
     // Send json response based of heads or tails from resulting coin flip
-    if (result == 'heads') {
-        res.json({"flip":"heads"});
-    } 
-    if (result == 'tails') {
-        res.json({"flip":"tails"});
-    } 
+    res.json({"flip": result});
 });
 
 someroutes.route('/app/flips/:number').get(function (req, res, next) { // Flip a coin multiple times and return the results
@@ -27,24 +22,42 @@ someroutes.route('/app/flips/:number').get(function (req, res, next) { // Flip a
     // Set up variable for number of coin flips, array of results, and counted results
 	const flips = req.params.number;
     const results = coins.coinFlips(flips);
-    const counted = coins.countFlips(coinFlips(flips));
+    const counted = coins.countFlips(coins.coinFlips(flips));
 	// Flip coin "flips" number of times using the coinFlips function, send json response of results
     res.json({"raw":results, "summary":counted});
-    // send json response of results
+});
+
+someroutes.route('/app/flips/coins/').post(function (req, res, next) {
+    // Respond with status 200
+    res.statusCode = 200;
+    // Set up variable for number of coin flips and counted results, then send json response
+    var results = coins.coinFlips(req.body.number)
+    var counted = coins.countFlips(results)
+    res.json({"raw":results, "summary":counted});
 });
 
 someroutes.route('/app/flip/call/heads').get(function (req, res, next) {  // Flip a coin, call heads, compare result
     // Respond with status 200
     res.statusCode = 200;
-       // Use flipACoin function, send json response of results
-    res.json(coins.flipACoin('heads'));
+    // Use flipACoin function, send json response of results
+    const result = coins.flipACoin(req.params.guess)
+    res.json(result);
 });
 
 someroutes.route('/app/flip/call/tails').get(function (req, res, next) {  // Flip a coin, call heads, compare result
     // Respond with status 200
     res.statusCode = 200;
     // Use flipACoin function, send json response of results
-    res.json(coins.flipACoin('tails'));
+    const result = coins.flipACoin(req.params.guess)
+    res.json(result);
 });
+
+someroutes.route('/app/flip/call/').post(function (req, res, next) {
+    // Respond with status 200
+    res.statusCode = 200;
+    // Use flipACoin function, send json response of results
+    const result = coins.flipACoin(req.body.guess)
+    res.json(result);
+})
 
 module.exports = someroutes;
